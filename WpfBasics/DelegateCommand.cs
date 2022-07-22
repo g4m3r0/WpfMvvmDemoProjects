aca1832a8c30073c2026048b4f93d040fd3662d1
Lucas <9322695+g4m3r0@users.noreply.github.com>
2022-07-22 19:27:43 +0200

    Refactor

--- WpfBasics/DelegateCommand.cs
+++ WpfBasics/DelegateCommand.cs
@@ -4,18 +4,18 @@ namespace WpfBasics
 {
     public class DelegateCommand : ICommand
     {
-        readonly Action<object> execute;
-        private readonly Predicate<object> canExecute;
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
 
-        public DelegateCommand(Predicate<object> canExecute, Action<object> execute) => (this.canExecute, this.execute) = (canExecute, execute);
+        public DelegateCommand(Predicate<object> canExecute, Action<object> execute) => (this._canExecute, this._execute) = (canExecute, execute);
 
         public DelegateCommand(Action<object> execute) : this(null, execute) { }
 
         public void RaiseCanExecuteChanged() => this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
 
-        public bool CanExecute(object? parameter) => this.canExecute?.Invoke(parameter) ?? true;
+        public bool CanExecute(object? parameter) => this._canExecute?.Invoke(parameter) ?? true;
 
-        public void Execute(object? parameter) => this.execute?.Invoke(parameter);
+        public void Execute(object? parameter) => this._execute?.Invoke(parameter);
 
         public event EventHandler? CanExecuteChanged;
     }
