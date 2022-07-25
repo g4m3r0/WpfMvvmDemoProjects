c21a7a6e401a4a1995050e412677921ec45bf49a
Lucas <9322695+g4m3r0@users.noreply.github.com>
2022-07-25 19:37:51 +0200

    Add UserControl Demo

---
+++ WpfMvvmUserControls/MaxLengthTextBoxUserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMvvmUserControls
+{
+    /// <summary>
+    /// Interaction logic for MaxLengthTextBoxUserControl.xaml
+    /// </summary>
+    public partial class MaxLengthTextBoxUserControl : UserControl
+    {
+        public MaxLengthTextBoxUserControl()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public string Caption { get; set; }
+        public int MaxLength { get; set; }
+    }
+}
