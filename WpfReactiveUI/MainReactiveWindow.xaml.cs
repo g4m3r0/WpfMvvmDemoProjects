2225cfcfc2dc89389da8644ab82c1f5513a96515
Lucas <9322695+g4m3r0@users.noreply.github.com>
2023-05-10 16:56:16 +0200

    Add reactive UI WPF example

---
+++ WpfReactiveUI/MainReactiveWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ReactiveUI;
+
+namespace WpfReactiveUI
+{
+    /// <summary>
+    /// Interaction logic for MainReactiveWindow.xaml
+    /// </summary>
+    public partial class MainReactiveWindow
+    {
+        public MainReactiveWindow()
+        {
+            InitializeComponent();
+
+            InitializeComponent();
+            ViewModel = new MainWindowViewModel();
+
+            // We create our bindings here. These are the code behind bindings which allow 
+            // type safety. The bindings will only become active when the Window is being shown.
+            // We register our subscription in our disposableRegistration, this will cause 
+            // the binding subscription to become inactive when the Window is closed.
+            // The disposableRegistration is a CompositeDisposable which is a container of 
+            // other Disposables. We use the DisposeWith() extension method which simply adds 
+            // the subscription disposable to the CompositeDisposable.
+            this.WhenActivated(disposableRegistration =>
+            {
+                // Notice we don't have to provide a converter, on WPF a global converter is
+                // registered which knows how to convert a boolean into visibility.
+                this.OneWayBind(ViewModel,
+                    viewModel => viewModel.IsAvailable,
+                    view => view.searchResultsListBox.Visibility)
+                    .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(ViewModel,
+                    viewModel => viewModel.SearchResults,
+                    view => view.searchResultsListBox.ItemsSource)
+                    .DisposeWith(disposableRegistration);
+
+                this.Bind(ViewModel,
+                    viewModel => viewModel.SearchTerm,
+                    view => view.searchTextBox.Text)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
---
+++ WpfReactiveUI/NugetDetailsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using NuGet.Protocol.Core.Types;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfReactiveUI
+{
+    // This class wraps our NuGet model object into a ViewModel and allows
+    // us to have a ReactiveCommand to open the NuGet package URL.
+    public class NugetDetailsViewModel : ReactiveObject
+    {
+        private readonly IPackageSearchMetadata _metadata;
+        private readonly Uri _defaultUrl;
+
+        public NugetDetailsViewModel(IPackageSearchMetadata metadata)
+        {
+            _metadata = metadata;
+            _defaultUrl = new Uri("https://git.io/fAlfh");
+            OpenPage = ReactiveCommand.Create(() =>
+            {
+                Process.Start(new ProcessStartInfo(this.ProjectUrl.ToString())
+                {
+                    UseShellExecute = true
+                });
+            });
+        }
+
+        public Uri IconUrl => _metadata.IconUrl ?? _defaultUrl;
+        public string Description => _metadata.Description;
+        public Uri ProjectUrl => _metadata.ProjectUrl;
+        public string Title => _metadata.Title;
+
+        // ReactiveCommand allows us to execute logic without exposing any of the 
+        // implementation details with the View. The generic parameters are the 
+        // input into the command and its output. In our case we don't have any 
+        // input or output so we use Unit which in Reactive speak means a void type.
+        public ReactiveCommand<Unit, Unit> OpenPage { get; }
+    }
+}
